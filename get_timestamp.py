fdd66551886b499cdc0971e64ceb0f426e55ed0d
Glebov Daniil <dania.57.v@gmail.com>
2021-01-12 18:49:36 +0300

    Fix script

--- get_timestamp.py
+++ get_timestamp.py
@@ -1,3 +1,3 @@
 import os
 
-print(sorted([path for path in os.listdir("./data") if path.startswith("Linear")])[-1].split("_")[1].split(".")[0])
+print(sorted([path for path in os.listdir("./../data") if path.startswith("Linear")])[-1].split("_")[1].split(".")[0])
