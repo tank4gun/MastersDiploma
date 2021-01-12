9bde207bf1fdff1452ea9662f22903c241e35367
Glebov Daniil <dania.57.v@gmail.com>
2021-01-12 15:50:22 +0300

    Add simple flask app

---
+++ app/create_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, url_for, request, render_template
+from markupsafe import escape
+from argparse import ArgumentParser
+
+
+app = Flask(__name__)
+from views import *
+
+
+def create_app(deploy_version):
+    app.config['deploy_version'] = deploy_version
+    print('Passed item: ', app.config['deploy_version'])
+    return app
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-deploy_version')
+    args = parser.parse_args()
+    app = create_app(args.deploy_version)
+    app.run()
+
---
+++ app/views.py
@@ -0,0 +1,37 @@
+from create_app import app
+from flask import Flask, url_for, request, render_template
+import pandas as pd
+import mleap.sklearn.pipeline
+import mleap.sklearn.base
+from mleap.sklearn.preprocessing.data import FeatureExtractor
+
+import pandas as pd
+import math
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+input_names = ["age", "sex", "bmi", "bp", "s1", "s2", "s3", "s4", "s5", "s6"]
+
+
+@app.route('/')
+def index():
+    return render_template('input_screen.html')
+
+
+@app.route("/send_data", methods=['POST'])
+def send_data():
+    app.logger.debug("Got request data %s", request.form)
+    new_regr = LinearRegression().deserialize_from_bundle("../data", "Linear-regression_{}.node".format(app.config['deploy_version']))
+    with open("../data/Linear-regression_{version}.node/Coef_{version}.txt".format(version=app.config['deploy_version']), "r") as coef_file:
+    	means = np.array(list(map(float, coef_file.readline().split())))
+    	stds = np.array(list(map(float, coef_file.readline().split())))
+    app.logger.debug("Got mean data {}, std data {}".format(means, stds))
+    input_vector = np.array([float(request.form[name]) for name in input_names])
+    print(input_vector, means)
+    normalized_vector = (input_vector - means) / (stds * math.sqrt(442))
+    return "Model prediction is {prediction}".format(prediction=new_regr.predict([normalized_vector])[0][0])
+    
